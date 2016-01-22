613f89d1d4c2305ee342423873646174b5429426
Doug Greene <doug@l4mobile.com>
2016-01-21 17:48:16 -0800

    Simplified data points

--- src/graphify.js
+++ src/graphify.js
@@ -17,30 +17,20 @@
         this.createGraph();
 
         this.loadDataInGraph = function() {
+
+            var labelX = options.values.x;
+
             var tempData = {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
-                datasets: [
-                    {
-                        label: "My First dataset",
-                        fillColor: "rgba(220,220,220,0.2)",
-                        strokeColor: "rgba(220,220,220,1)",
-                        pointColor: "rgba(220,220,220,1)",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(220,220,220,1)",
-                        data: [65, 59, 80, 81, 56, 55, 40]
-                    },
-                    {
-                        label: "My Second dataset",
-                        fillColor: "rgba(151,187,205,0.2)",
-                        strokeColor: "rgba(151,187,205,1)",
-                        pointColor: "rgba(151,187,205,1)",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(151,187,205,1)",
-                        data: [28, 48, 40, 19, 86, 27, 90]
-                    }
-                ]
+                labels: [0, 5, 10],
+                datasets: [{
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: [20, 40, 50]
+                }]
             };
             var ctx = $("#" + options.chartIdName).get(0).getContext("2d");
 
@@ -58,13 +48,8 @@
         dataType: "deterministic",          // chart type
         graphType: "geometric",             // graph type
         values: {                           // data to pass
-            average:    null,
-            deviation:  null,
-            maximum:    null,
-            mean:       null,
-            minimum:    null,
-            x:          null,
-            y:          null
+            x: null,
+            y: null
         }
     };
 
