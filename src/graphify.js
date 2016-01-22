e35d259ecb86b8207bc9a6bf80ab7388d3cd51be
Doug Greene <doug@l4mobile.com>
2016-01-21 18:10:37 -0800

    Took passed data to plug into the rendered chart, created a watch (that doesn't do much yet).

--- src/graphify.js
+++ src/graphify.js
@@ -6,6 +6,7 @@
         var options = $.extend( {}, $.fn.graphify.defaults, userOptions );
         var elem =  this.selector;
 
+        // create the elements
         this.createGraph = function() {
             $(elem).val(JSON.stringify(options.values));
             $(elem).after(
@@ -14,36 +15,54 @@
                 "</div>"
             );
         };
-        this.createGraph();
 
+        // load the data and render it
         this.loadDataInGraph = function() {
-
-            var labelX = options.values.x;
-
-            var tempData = {
-                labels: [0, 5, 10],
+            var renderData = {
+                labels: options.values.x,
                 datasets: [{
-                    fillColor: "rgba(151,187,205,0.2)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: [20, 40, 50]
+                    data: options.values.y,
+                    fillColor: options.fillColor,
+                    strokeColor: options.strokeColor,
+                    pointColor: options.pointColor,
+                    pointStrokeColor: options.pointStrokeColor,
+                    pointHighlightFill: options.pointHighlightFill,
+                    pointHighlightStroke: options.pointHighlightStroke,
                 }]
             };
-            var ctx = $("#" + options.chartIdName).get(0).getContext("2d");
 
-            var myNewChart = new Chart(ctx).Line(tempData, null);
-            console.log(myNewChart);
+            var ctx = $("#" + options.chartIdName).get(0).getContext("2d");
+            new Chart(ctx).Line(renderData, options.chartOptions);
         };
-        this.loadDataInGraph();
+
+        // fusebox
+        this.makeMeAChart = function() {
+            this.createGraph();
+            this.loadDataInGraph();
+        };
+        this.makeMeAChart();
+
+        // watcher
+        $(elem).on('change', function(e) {
+            alert('dont touch that yet!');
+        });
     };
 
     $.fn.graphify.defaults = {
+
+        // chart JS defaults
+        fillColor: "rgba(151,187,205,0.2)",
+        strokeColor: "rgba(151,187,205,1)",
+        pointColor: "rgba(151,187,205,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)",
+
+        // graphify defaults
         chartClassName: "data-chart",       // custom class name (for styling)
         chartHeight: 400,                   // chart height
         chartIdName: "dataChart",           // custom id name
+        chartOptions: null,                 // custom options for Chart JS 'options'
         chartWidth: 400,                    // chart width
         dataType: "deterministic",          // chart type
         graphType: "geometric",             // graph type
